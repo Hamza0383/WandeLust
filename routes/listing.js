695cf0ba6ce9d1157209bfa0b821c1e706aab447
Hamza <m.hamzakhaan@gmail.com>
2024-05-21 22:18:48 +0500

    express router added

---
+++ routes/listing.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const { listingSchema } = require("../schema.js");
+const Listing = require("../models/listing");
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  let errMsg = error.details.map((el) => el.message).join(",");
+  if (error) {
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+router.get(
+  "/",
+  wrapAsync(async (req, res) => {
+    let allListing = await Listing.find({});
+    res.render("listing/index.ejs", { allListing });
+  })
+);
+router.get("/new", (req, res) => {
+  res.render("listing/new.ejs");
+});
+router.get(
+  "/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id).populate("reviews");
+    res.render("listing/show.ejs", { listing });
+  })
+);
+router.post(
+  "/",
+  wrapAsync(async (req, res) => {
+    let listing = new Listing(req.body.listing);
+    await listing.save();
+    res.redirect("/listing");
+  })
+);
+router.get(
+  "/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listing/edit.ejs", { listing });
+  })
+);
+router.put(
+  "/:id",
+  wrapAsync(async (req, res, next) => {
+    if (!req.body.listing) {
+      throw new Error(400, "Send valid Data for Listing");
+    }
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listing/${id}`);
+  })
+);
+router.delete(
+  "/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listing");
+  })
+);
+module.exports = router;
